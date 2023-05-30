772cd3b94b269edd53847a037c77bf07e6743a60
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2023-05-30 18:55:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-camera = document.getElementById("camera");
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/L5MzVk9qs/model.json', modelLoaded);
 Webcam.set({
     width: 350,
     height: 300,
@@ -7,13 +5,27 @@ Webcam.set({
     png_quality: 90
 });
 Webcam.attach('#camera');
-
+camera = document.getElementById("camera");
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/L5MzVk9qs/model.json', modelLoaded);
 function ImageCapture() {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML= '<img id="capture_img" src="' + data_uri +'">'     
+        document.getElementById("result").innerHTML="<img id='capturedImg' src='" + data_uri +"'>" 
     })
 }
 console.log("ml5 version :", ml5.version);
 function modelLoaded(){
-    console.log("Model has now been loaded.")
+    console.log("Model has now been loaded.");
+}
+function Identify(){
+    img = document.getElementById("capturedImg");
+    classifier.classify(img, gotResult);
+}
+function gotResult( error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("Result_member").innerHTML = "The Family Memebr is " + results.object[0].label;
+        document.getElementById("Result_accuracy").innerHTML = "The Acurracy is " + results.object[0].confidence.toFixed(3);
+    }
 }
